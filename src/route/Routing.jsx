c1014b49876abbbdc6bdc658bebcba49b86c73db
Impactmindz-Tech <impactmindztechsolutions@gmail.com>
2024-06-10 13:38:09 +0530

    code done

--- src/route/Routing.jsx
+++ src/route/Routing.jsx
@@ -5,6 +5,8 @@ import Login from '../page/auth/Login'
 import First from '../components/sujjestion/First'
 import Second from '../components/sujjestion/Second'
 import UpdateUnavailability from '@/page/dashboard/UpdateUnavailability'
+import UpdateProfilePicture from '@/page/dashboard/UpdateProfilePicture'
+import UpdateAddress from '@/page/dashboard/UpdateAddress'
 
 const Routing = () => {
     return (
@@ -15,6 +17,8 @@ const Routing = () => {
                 <Route path='/first' element={<First />} />
                 <Route path='/second' element={<Second />} />
                 <Route path='/UpdateUnavailability' element={<UpdateUnavailability />} />
+                <Route path='/update_profile_picture' element={<UpdateProfilePicture />} />
+                <Route path='/update_address' element={<UpdateAddress />} />
             </Routes>
         </BrowserRouter>
     )
